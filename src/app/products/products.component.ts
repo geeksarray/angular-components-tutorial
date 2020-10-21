525de448d6d5f0fe956b2eed01bab797efb6f2ad
geeksarray <36873556+geeksarray@users.noreply.github.com>
2020-10-21 08:10:28 +0530

    added products component

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  products : any[] =[ {  
+      "ProductID" :1,  
+      "ProductName": "Solid State Drive",  
+      "CategoryId":100,
+      "AvilableQty":50,
+      "ReorderQty":30
+    },  
+    {  
+      "ProductID" : 2,  
+      "ProductName": "Monitor",  
+      "CategoryId":101,
+      "AvilableQty":20,
+      "ReorderQty":10       
+    },  
+    {  
+      "ProductID" : 3,  
+      "ProductName" : "LED Display Monitor",  
+      "CategoryId":100,
+      "AvilableQty":5,
+      "ReorderQty":10      
+    }, 
+    {  
+      "ProductID" : 4,  
+      "ProductName": "San Disk 64 GB Pen Drive",  
+      "CategoryId":105,
+      "AvilableQty":35,
+      "ReorderQty":80      
+    }, 
+    {  
+      "ProductID" : 5,  
+      "ProductName": "Think Pad Ultra Dock",  
+      "CategoryId":105,
+      "AvilableQty":25,
+      "ReorderQty":20      
+    }  
+  ]
+}
